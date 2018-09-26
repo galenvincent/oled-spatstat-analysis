d7c7a343752c977cf9bcc459529a80624ea2305d
Galen Vincent <galenvincent@mymail.mines.edu>
2018-09-26 10:43:06 -0600

    Box Size Effect Code

--- box_size_effect.R
+++ box_size_effect.R
@@ -2,13 +2,13 @@
 # Written to be tested locally, run on HPC for full analysis
 library(rapt)
 library(parallel)
+library(data.table)
 
 #### Some tests to start ####
 
 # upload 2 rcps
-under <- read.rcp('~/scratch/Rcode/FinalConfigs/FinalConfig1','~/scratch/Rcode/systems/system1',scaleUp = TRUE,newRadius = 0.5)
-#over <- read.rcp('~/Research/point_patterns/Final/FinalConfig2','~/Research/point_patterns/Final/system2',scaleUp = TRUE,newRadius = 0.5)
-
+under <- read.rcp('~/Research/point_patterns/Final/FinalConfig1','~/Research/point_patterns/Final/system1',scaleUp = TRUE,newRadius = 0.5)
+#under <- read.rcp('~/scratch/Rcode/FinalConfigs/FinalConfig1','~/scratch/Rcode/systems/system1',scaleUp = TRUE,newRadius = 0.5)
 #under.rrl <- pK3est(0.06,under,2000,nrval=200,correction="trans",anom=TRUE)
 #envPlot(under.rrl[[1]],ylim=c(-9,9))
 
@@ -16,64 +16,47 @@ under <- read.rcp('~/scratch/Rcode/FinalConfigs/FinalConfig1','~/scratch/Rcode/s
 #cluster.1.k <- anomK3est(cluster.1[[1]],under.rrl,correction="trans")
 #lines(cluster.1.k,col="black",lwd=2)
 
-#### 50,000 RRL on all of the different box conditions ####
-# 1. Create all of the different box sizes
-cube.15x15 <- stitch.size(under,boxSize=c(15,15,15))
-a <- pK3est(0.06,cube.15x15,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/cube15x15.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/cube15x15toSub.csv")
-rm(cube.15x15, a)
+#### create a 60x60x60 pattern of clusters, subsample it into different sized
+# patterns, and run the functions on them (with correct subtractions) ####
 
-cube.20x20 <- stitch.size(under,boxSize=c(20,20,20))
-a <- pK3est(0.06,cube.20x20,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/cube20x20.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/cube20x20toSub.csv")
-rm(cube.20x20, a)
+# Doing 40 realizations of the same type of clustering, split the rcp patterns
+# into 2 different sets. The under patterns and the over patterns
+under.nums <- seq(1,40,1)
+over.nums <- seq(41,80,1)
 
-cube.30x30 <- stitch.size(under,boxSize=c(30,30,30))
-a <- pK3est(0.06,cube.30x30,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/cube30x30.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/cube30x30toSub.csv")
-rm(cube.30x30, a)
+toSub <- fread('~/Research/box_size_effect/RRLtoSub1.csv',drop=1)
+sample <- fread('~/Research/box_size_effect/RRL1.csv',select=2)
+env <- fread('~/Research/box_size_effect/RRL1.csv',drop=1)
 
-cube.40x40 <- stitch.size(under,boxSize=c(40,40,40))
-a <- pK3est(0.06,cube.40x40,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/cube40x40.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/cube40x40toSub.csv")
-rm(cube.40x40, a)
 
-cube.60x60 <- stitch.size(under,boxSize=c(60,60,60))
-a <- pK3est(0.06,cube.60x60,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/cube60x60.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/cube60x60toSub.csv")
-rm(cube.60x60, a)
+#loop
+for(i in(1:40)){
+i <- 1
+#upload
+  under <- read.rcp(paste('~/Research/point_patterns/Final/FinalConfig',toString(under.nums[i]),sep=""),paste('~/Research/point_patterns/Final/system',toString(under.nums[i]),sep=""),scaleUp = TRUE,newRadius = 0.5)
+  over <- read.rcp(paste('~/Research/point_patterns/Final/FinalConfig',toString(over.nums[i]),sep=""),paste('~/Research/point_patterns/Final/system',toString(over.nums[i]),sep=""),scaleUp = TRUE,newRadius = 0.5)
 
-box.60x60x10 <- stitch.size(under,boxSize=c(60,60,10))
-a <- pK3est(0.06,box.60x60x10,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/box.60x60x10.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/box.60x60x10toSub.csv")
-rm(box.60x60x10, a)
+  under.big <- stitch(under,c(3,3,3))
+  over.big <- stitch(over,c(3,3,3))
+#make cluster
+  cluster <- makecluster(under.big,over.big,0.5,0.5,type="cr",superfast = TRUE,cr=2)
+  
+#test on each of the different cluster sizes below here
+  
+#cut it down to size
+  cluster.cut <- subSquare(cluster[[1]],c(60,60,60))
+  
+#test
+  result <- anomK3est(cluster.cut,toSub,max(sample),200,correction="trans")
 
-box.60x60x15 <- stitch.size(under,boxSize=c(60,60,15))
-a <- pK3est(0.06,box.60x60x15,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/box.60x60x15.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/box.60x60x15toSub.csv")
-rm(box.60x60x15, a)
+#output
+  fwrite(result,'~/Research/box_size_effect/result1_1.csv',sep=',')
 
-box.60x60x20 <- stitch.size(under,boxSize=c(60,60,20))
-a <- pK3est(0.06,box.60x60x20,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/box.60x60x20.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/box.60x60x20toSub.csv")
-rm(box.60x60x20, a)
+#clear memory
+  rm(under,over,under.big,over.big,cluster,cluster,cluster.cut,result)
+  gc()
 
-box.60x60x30 <- stitch.size(under,boxSize=c(60,60,30))
-a <- pK3est(0.06,box.60x60x30,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/box.60x60x30.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/box.60x60x30toSub.csv")
-rm(box.60x60x30, a)
+#repeat
+}
 
-box.60x60x40 <- stitch.size(under,boxSize=c(60,60,40))
-a <- pK3est(0.06,box.60x60x40,50000,nrval=200,correction="trans",anom=TRUE)
-write.csv(a[[1]],file="~/scratch/Rcode/box.60x60x40.csv")
-write.csv(a[[2]],file="~/scratch/Rcode/box.60x60x40toSub.csv")
-rm(box.60x60x40, a)
+
