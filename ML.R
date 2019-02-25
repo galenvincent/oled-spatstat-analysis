24042c199bab07e1165cbcd59575f1a118b6bdb3
Galen Vincent <galenvincent@ck182-29.mines.edu>
2019-02-25 15:22:37 -0700

    Updated ML code

--- ML.R
+++ ML.R
@@ -2,6 +2,8 @@
 
 library(data.table)
 library(caret)
+library(parallel)
+library(doParallel)
 
 dep <- fread("~/Research/PCA/190216_pca.csv",select = c(1:4))
 names(dep) <- c("r","den","rb","gb")
@@ -13,14 +15,14 @@ data <- data.frame(met, dep)
 data$rw <- (data$r^4 + 6* (data$r^2) *(data$rb * data$r)^2 + 3 *(data$rb * data$r)^4)/(data$r^3 + 3*data$r*(data$rb * data$r)^2)
 data$sigma <- data$rb * data$r
 
-p = 0.1
+p = 1
 samp <- sample(1:nrow(dep),round(nrow(dep)*p),replace = FALSE)
 
 trainSet <- data[samp,]
 testSet <- data[-samp,] 
 
 
-out <- 'rw'
+out <- 'den'
 predictors <- c("Km","Rm","Rdm","Rddm","Kdm")
 
 models <- list()
@@ -35,9 +37,14 @@ models[[3]] <- train(trainSet[,predictors], trainSet[,out],
                trControl = trainControl("cv", number = 10),
                tuneLength = 10)
 models[[4]] <- train(trainSet[,predictors],trainSet[,out],method = "brnn", trControl = trainControl("cv", number = 10))
-models[[5]] <- train(trainSet[,predictors],trainSet[,out],method = "parRF", trControl = trainControl("cv", number = 10))
 
-ypoly <- lm(trainSet[,out] ~ poly(trainSet[,predictors[1]],
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
+models[[5]] <- train(trainSet[,predictors],trainSet[,out],method = "parRF", trControl = trainControl("cv", number = 10))
+stopCluster(cl)
+registerDoSEQ()
+
+poly <- lm(trainSet[,out] ~ poly(trainSet[,predictors[1]],
                                  trainSet[,predictors[2]],
                                  trainSet[,predictors[3]],
                                  trainSet[,predictors[4]],
@@ -48,13 +55,17 @@ names(models) <- c("lm","knn","elastic-net","brnn","rf")
 
 compall <- extractPrediction(models)#, testX = testSet[,predictors], testY = testSet[,out])
 comp <- split(compall, compall$model)
-comp[[4]] <- data.frame(obs = trainSet[,out], pred = predict(poly), model = "polylm", dataType = "Training", object = "poly")
+comp[[length(models) + 1]] <- data.frame(obs = trainSet[,out], pred = predict(poly), model = "polylm", dataType = "Training", object = "poly")
+names(comp)[length(models) + 1] <- "polylm"
 
 lapply(comp, function(x){
+  ds <- defaultSummary(x)
+  
   plot(x$obs, x$pred, main = toString(x$model[1]), xlab = "Actual Value", ylab = "Predicted Value")
   abline(0,1, col = "red", lwd = 2)
-  defaultSummary(x)
+  return(ds)
 })
+
 
 plot(varImp(models[[1]]))
 plot(varImp(models[[2]]))
